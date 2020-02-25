585b494c6249ea46fc26f974908186c5d2c0c1e0
martin chan <martinchan994@gmail.com>
2020-02-25 22:55:48 +0100

    Added test for add and remove recipe

--- unittests/test_app.py
+++ unittests/test_app.py
@@ -1,11 +1,33 @@
 def test_get_all_data(client):
-    result = client.get("/")
-    assert result.json['success']
-    assert result.json['code'] == 200
-    assert result.json['result']['recipes']
+	result = client.get("/")
+	assert result.json['success']
+	assert result.json['code'] == 200
+	assert result.json['result']['recipes']
 
 def test_get_all_recipes(client):
-    result = client.get("/recipes")
-    assert result.json['success']
-    assert result.json['code'] == 200
-    assert len(result.json['result'])
+	result = client.get("/recipes")
+	assert result.json['success']
+	assert result.json['code'] == 200
+	assert len(result.json['result'])
+
+def test_add_recipe(client):
+	result = client.post('/addrecipe', json={
+		'password': 'Troglodon5986',
+		'name': 'testrecipe',
+		'servings': 2,
+		'description': 'This is a test recipe',
+		'ingredients': [
+			{'name': 'testIngredient',
+			'amount': 1,
+			'unit': 'dl'}
+		]
+	})
+	assert result.json['success']
+	assert result.json['code'] == 201
+
+def test_remove_recipe(client):
+	result = client.put('/removerecipe', json={
+	'password': 'Troglodon5986',
+	'name': 'testrecipe'
+	})
+	assert result.json['success']
