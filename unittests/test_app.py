c25cdfe4f745df6f602b3186c12b9149c7290fd3
martin chan <martinchan994@gmail.com>
2020-02-26 22:11:12 +0100

    Added test for adding recipe without password

--- unittests/test_app.py
+++ unittests/test_app.py
@@ -10,7 +10,7 @@ def test_get_all_recipes(client):
 	assert result.json['code'] == 200
 	assert len(result.json['result'])
 
-def test_add_recipe(client):
+def test_add_correct_recipe(client):
 	result = client.post('/addrecipe', json={
 		'password': 'Troglodon5986',
 		'name': 'testrecipe',
@@ -24,6 +24,20 @@ def test_add_recipe(client):
 	})
 	assert result.json['success']
 	assert result.json['code'] == 201
+
+def test_add_recipe_without_password(client):
+	result = client.post('/addrecipe', json={
+		'name': 'testrecipe2',
+		'servings': 2,
+		'description': 'This is another test recipe',
+		'ingredients': [
+			{'name': 'testIngredient',
+			'amount': 1,
+			'unit': 'dl'}
+		]
+	})
+	assert not result.json['success']
+	assert result.json['code'] == 401
 
 def test_remove_recipe(client):
 	result = client.put('/removerecipe', json={
